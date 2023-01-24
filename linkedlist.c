d7da62df2e5c4cd65590500ab11bc19d7f37f728
HanruC <chen.hanru@northeastern.edu>
2023-01-24 17:16:08 -0500

    3

--- linkedlist.c
+++ linkedlist.c
@@ -35,14 +35,26 @@ node_t* build_list(int* wins, int* years, int size) {
         return head;
 }
 
+void printList(node_t* head){
+    if (head == NULL){
+        return;
+    }
+    node_t* pointer = head;
+    printf("%d, %d wins\n", pointer->year, pointer->wins);
+    pointer = pointer->next; 
+}
+
 // TODO: Write your functions here
 // There should be 1.) create_list 2.) print_list 3.) free_list
 // You may create as many helper functions as you like.
 
 int main()
 {
-    //int test_years[5] = {2018, 2017, 2016, 2015, 2014};
-    //int test_wins[5] = {108, 93, 93, 78, 71};
+    int test_years[5] = {2018, 2017, 2016, 2015, 2014};
+    int test_wins[5] = {108, 93, 93, 78, 71};
+
+    newlist = build_list(test_years, test_wins, 5);
+    printList(newlist);
 
     // TODO: Implement me!
 
