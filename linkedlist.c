bbff4d84a9638cd4eb2798a9800cd28ce24c96e4
HanruC <chen.hanru@northeastern.edu>
2023-01-24 22:00:16 -0500

    step 4 finished

--- linkedlist.c
+++ linkedlist.c
@@ -7,6 +7,32 @@ typedef struct node {
     struct node* next;
 }node_t;
 
+typedef struct lst {
+    int size;
+    node_t* head;
+}lst_t;
+
+// create a list to store each node in the list. 
+lst_t* newList(){
+    lst_t* newList = (node_t*)malloc(sizeof(lst_t));
+    if (newList == NULL){
+        return NULL;
+    }
+    newList->head =NULL;
+    newList->size =0;
+    return newList;
+}
+
+// now we need to add the node into the list, which is inserting the nodes in the front. 
+//set the head of the list as the newNode 
+//then newNode->next will be the list head 
+void add_nodes(lst_t* list, int year, int wins){
+    node_t* newNode = makeNode(year, wins, NULL);
+    newNode->next = list->head; //1. new->next:NULL (list->head was set to be null at first) 2. new->next: the first makenode. 3. new->next: the second makenode. 
+    list->head = newNode; // 1. list->head set to be the first makenode. 2. the second makenode. 3. the third makenode. 
+    // 5 4 3 2 1 from the end insert the front. 
+    list->size++
+}
 node_t* makeNode(int wins, int year, node_t* next) {
     node_t* newNode = (node_t*)malloc(sizeof(node_t)); 
     if (newNode == NULL){
@@ -61,13 +87,17 @@ void free_list(node_t* head){
 }
 
 
-int find_year(node_t* head, int year_list){
-    node_t* pointer = head; 
-    if (pointer != NULL){
-        return find_year(pointer->year) 
+note_t* find_year(lst_t* list, int year){
+    node_t* pointer = list->head; 
+    if (pointer == NULL){
+        return NULL;
     }
-    
-    
+    while (pointer != NULL){
+        if (pinter->year == year){
+            return pointer;
+        }
+        pointer = pointer->next;
+    }
 }
 
 // TODO: Write your functions here
@@ -79,10 +109,21 @@ int main()
     int test_years[5] = {2018, 2017, 2016, 2015, 2014};
     int test_wins[5] = {108, 93, 93, 78, 71};
 
-    node_t* newlist = build_list(test_years, test_wins, 5);
-    printList(newlist);
+    list_t* newlist = create_list();
+    for (int i = 0; i < 5; i++) {
+        add_node(newlist, test_years[i], test_wins[i]);
+    }
+    print_list(newlist->head);
 
-    // TODO: Implement me!
+    int search_year = 2016;
+    node_t* found_year = find_year(newlist, search_year);
+    if (found_year != NULL) {
+        printf("Year: %d, Wins: %d\n", found_year->year, found_year->wins);
+    } else {
+        printf("Year %d not found in the list.\n", search_year);
+    }
 
+    free_list(newlist->head);
+    free(newlist);
     return 0;
 }
